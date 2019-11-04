c25f4cf5a32f62461802468403066bc07978b60f
iordakis <iordakis@gmail.com>
2019-11-04 12:06:48 +0200

    Update instance-limiter.py

--- instance-limiter.py
+++ instance-limiter.py
@@ -3,24 +3,25 @@
 import sys
 import psutil
 
+targetProgram = 'target-program.exe' # your target program
+procList = []
+proccessCount = 1
 
-proclist = []
-proccesscount = 1
-for proc in psutil.process_iter(): # appending current running processes to list
-    if proc.name() == 'target-program.exe':
-        proclist.append(proc)
+for proc in psutil.process_iter(): # appending current target running processes to list
+    if proc.name() == targetProgram:
+        procList.append(proc)
 
-for proccesscount, item in enumerate(proclist): # counting total instances of the target executable
-    print('Process found: ', proccesscount)
+for proccessCount, item in enumerate(procList): # counting total instances of the target program
+    print('Process found: ', proccessCount)
 
-if proccesscount >= 2:
+if proccessCount >= 2: # prevents running more than one instance of the target program
     print("Only one instance allowed to run!")
     time.sleep(3)
     sys.exit() # this stops the python script. replace with your desired command
 
-proclist = str(proclist) # converting list to string after counting list elements above
+procList = str(procList) # converting list to string after counting list elements above
 
-if 'target-program.exe' not in proclist: # denies changing executable name that can avoid instance counting above
-    print("Changing executable name not allowed!")
+if targetProgram not in procList: # prevents changing program file name that can avoid instance counting above
+    print("Changing program file name not allowed!")
     time.sleep(3)
     sys.exit() # this stops the python script. replace with your desired command
